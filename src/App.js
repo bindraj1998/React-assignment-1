7f4563aa7db888bd2d11f13ba4273626f0f67ef3
bindraj1998 <rajkumarbind555@gmail.com>
2022-05-18 19:18:32 -0800

    Assignment 1 completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from './components/Data';
+function App() {
+  return (
+    <div >
+      <Data></Data>
+       {/* <p>hhh</p> */}
+        </div>
+  );
+}
+
+export default App;
---
+++ src/components/Data.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const data = () => {
+  return (
+    <div style={{marginLeft: '200px',marginTop:'100px'}}>
+        {/* <p>Hello</p> */}
+        <h2  >Mobile Operating System</h2>
+        
+         <ul style={{listStyle:'disc'}}>
+             <li>Android</li>
+             <li>Blckberry</li>
+             <li>Iphone</li>
+             <li>Windows phone</li>
+          </ul>
+
+
+        <h2  >Mobile manufacturer</h2>
+         <ul >
+             <li style={{listStyle:'square'}}>Samsung</li>
+             <li style={{listStyle:'square'}}>HTC</li>
+             <li style={{listStyle:'disc'}}>Micromax</li>
+             <li style={{listStyle:'circle'}}>Apple</li>
+          </ul >
+
+         
+     
+    </div>
+  )
+}
+
+export default data
